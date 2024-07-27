0826a3741b43326a58f0f1e2b7a96712219eac5c
Venkatesh Prabhu <g.venkateshprabhu@outlook.com>
2024-07-27 07:27:49 +0530

    Made changes to etl.py

--- simple-etl-pipeline/etl.py
+++ simple-etl-pipeline/etl.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 SOURCE_PATH = "data/staging"
 TARGET_PATH = "data/transformed"
 FILE = "power_consumption.csv"
@@ -12,12 +11,12 @@ def extract(file):
 
 def transform(temp_df):
     temp_df["DateTime"] = pd.to_datetime(temp_df["DateTime"])
-    temp_df = temp_df.groupby(temp_df["DateTime"].dt.date).mean().drop(columns=["DateTime"], axis=1).rename(columns={"DateTime": "Date"}).reset_index()
+    temp_df = temp_df.groupby(temp_df["DateTime"].dt.date).mean().round(2).drop(columns=["DateTime"], axis=1).rename(columns={"DateTime": "Date"}).reset_index()
     return temp_df
 
 
 def load(temp_df):
-    temp_df.to_csv(f"{TARGET_PATH}/transformed_{FILE}", index=False)
+    temp_df.to_csv(f"{TARGET_PATH}/transformed_{FILE}", index=False, mode="w")
     print(f"Target Shape: {temp_df.shape}")
 
 
